4605288db00ea93a4e355c4c325c90313fe9e815
Oleksii Krasnovskyi <finchatticu33@gmail.com>
2022-06-28 22:35:23 +0300

    Initial project structure

---
+++ src/main/java/edu/demian/zinon/ZinonApplication.java
@@ -0,0 +1,13 @@
+package edu.demian.zinon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZinonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZinonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/demian/zinon/ZinonApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.demian.zinon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZinonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
